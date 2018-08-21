2875a5bdf1dbbae8c5fc7218253817f609e8c112
MatteoPontalti <matteo.pontalti@studenti.unitn.it>
2018-08-21 17:40:07 +0200

    test

--- server.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-
-    const server = app.listen(process.env.PORT || 5000 , () => {
-        console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-    });
-    return server;
-};
