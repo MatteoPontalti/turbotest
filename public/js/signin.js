452982eac5d19a87ac290c9276e72e582a96f138
MatteoPontalti <matteo.pontalti@studenti.unitn.it>
2018-08-22 11:23:07 +0200

    modifiche

---
+++ public/js/signin.js
@@ -0,0 +1,23 @@
+var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '306163354645-8pp6iga4tc0fn8trem3j3gapbnhaalco.apps.googleusercontent.com'
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('googleButton'));
+    });
+  };
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+          document.getElementById('name').innerText = "Signed in: " +
+              googleUser.getBasicProfile().getName();
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
