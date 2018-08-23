9cfdb86f24b896e1763f13510fe03b161f3f91c7
MatteoPontalti <matteo.pontalti@studenti.unitn.it>
2018-08-23 16:57:43 +0200

    aggiunto guest

--- public/js/signout.js
+++ public/js/signout.js
@@ -4,10 +4,10 @@
     auth2.signOut().then(function () {
       console.log('User signed out.');
       document.getElementById('name').innerText = "Signed Out"    });
-		document.getElementById('m').disabled=true;
+		//document.getElementById('m').disabled=true;
   }
 
 function signGuest() {
       document.getElementById('name').innerText = "Signed In Like Guest"    });
-		document.getElementById('m').disabled=false;
+		//document.getElementById('m').disabled=false;
   }
